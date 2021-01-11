ae533a6c4d6a68ee41ad4705e2f25a1f8937093c
Naeemkhan33 <ranker333@gmail.com>
2021-01-11 13:26:42 +0500

    added more tooltips

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AiOutlinePlus, AiOutlineStar, AiOutlineWindows } from 'react-icons/ai';
 import { BiBookContent, BiStats } from 'react-icons/bi';
+import { Popover } from 'react-bootstrap';
+import { OverlayTrigger } from 'react-bootstrap';
+
+const popover1 = (
+  <Popover id='key-1' className='rounded-xl  shadow-sm border-0'>
+    <div className='p-3'>
+      <div className='font-weight-bold'>
+        <span className='im-text-primary mr-1'>1.</span>
+        <span>Let’s Setting Up Your Profile</span>
+      </div>
+      <p className='text-black-50 mt-4 small'>
+        Let’s setting up your profile by uploading a profile picture, and then
+        you are ready to go!
+      </p>
+    </div>
+  </Popover>
+);
 
 export const FooterBar = () => {
   return (
@@ -47,19 +64,20 @@ export const FooterBar = () => {
           <span className='small'>Matches</span>
         </NavLink>
 
-        <NavLink
-          to='/profile'
-          type='button'
-          className='btn btn-footer-bar p-0 w-100 text-left rounded-0 d-flex flex-column align-items-center'
-        >
-          <span
-            className='btn-two rounded-pill d-flex align-items-center justify-content-center'
-            style={{ height: 18, minWidth: 18 }}
+        <OverlayTrigger trigger='click' placement='auto' overlay={popover1}>
+          <Link
+            type='button'
+            className='btn btn-footer-bar p-0 w-100 text-left rounded-0 d-flex flex-column align-items-center'
           >
-            <AiOutlinePlus size={11} />
-          </span>
-          <span className='small'>Profile</span>
-        </NavLink>
+            <span
+              className='btn-two rounded-pill d-flex align-items-center justify-content-center'
+              style={{ height: 18, minWidth: 18 }}
+            >
+              <AiOutlinePlus size={11} />
+            </span>
+            <span className='small'>Profile</span>
+          </Link>
+        </OverlayTrigger>
       </main>
     </div>
   );
