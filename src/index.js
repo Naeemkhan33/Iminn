0b3929fa516ba6949c82d3a736b0eb4106fac1b5
Naeemkhan33 <ranker333@gmail.com>
2021-01-17 20:08:22 +0500

    styles components

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-calendar/dist/Calendar.css';
-import './index.css';
-import './scroll.styles.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+// styles
+import './styles/font.styles.css';
+import './styles/color.styles.css';
+import './styles/index.css';
+import './styles/custom-components.css';
+import './styles/checkbox.styles.css';
+import './styles/calendar.styles.css';
+import './styles/chat.styles.css';
+import './styles/ratings-statistic.styles.css';
+import './styles/button.styles.css';
+import './styles/sidebar.styles.css';
+import './styles/footer.styles.css';
+import './styles/scroll.styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
